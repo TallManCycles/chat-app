08b70022dc1fc0d0aea0e9121039f0c0250197f3
tallmancycles <aaronday@live.com.au>
2023-03-06 20:23:12 +1000

    Added better styling

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,139 +1,137 @@
-import React, { useState } from 'react'
-import './Chat.css'
-import { Configuration, OpenAIApi } from 'openai'
+import React, { useState, useRef } from 'react';
+import './Chat.css';
+import { Configuration, OpenAIApi } from 'openai';
+import Prism from 'prismjs';
 
-const Chat = () => {
-    const [messages, setMessages] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
+const configuration = new Configuration({
+  apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
-    if (!process.env.REACT_APP_OPENAI_API_KEY) {
-        console.error("Missing API KEY in file");
-      } 
+function TextCompletion(model, prompt, maxTokens) {
+  return openai.createCompletion({
+    model,
+    prompt,
+    max_tokens: Number.parseInt(maxTokens),
+    temperature: 0,
+  });
+}
 
-    const configuration = new Configuration({
-        apiKey: process.env.REACT_APP_OPENAI_API_KEY,
-      });
+function ChatCompletion(model, message) {
+  return openai.createChatCompletion({
+    model,
+    messages: [{ role: 'user', content: message }],
+  });
+}
 
-    const openai = new OpenAIApi(configuration);
+function Chat() {
+  const [messages, setMessages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const addMessage = (message) => {
-        setMessages([...messages, message]);
-    };
+  const messageInputRef = useRef(null);
+  const sendButtonRef = useRef(null);
 
-    const handleClearMessages = () => {
-        setMessages([]);
+  function addMessage(message) {
+    setMessages(messages => [...messages, message]);
+  }
+
+  function clearMessages() {
+    setMessages([]);
+    setIsLoading(false);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      sendButtonRef.current.click();
+    }
+  }
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+
+    const input = event.target.elements.message;
+    const message = input.value;
+
+    const options = event.target.elements.models;
+    const model = options.value;
+
+    const tokens = event.target.elements.maxTokens;
+    const maxTokens = tokens.value || 500;
+
+    try {
+      let response;
+      if (model === 'gpt-3.5-turbo') {
+        response = await ChatCompletion(model, message);
+      } else {
+        response = await TextCompletion(model, message, maxTokens);
+      }
+
+      if (!response) {
+        console.log('response is invalid');
+      } else {
+        const messages = response.data.choices.map(choice => choice.text || choice.message.content);
+        messages.forEach(addMessage);
+        console.log(messages);
+      }
+    } catch (error) {
+      console.log(error.message);
     }
 
-    async function TextCompletion(openai, model, text, maxTokens, addMessage) {
-      try {
-          const response = await openai.createCompletion({
-            model: model,
-            prompt: text,
-            max_tokens: Number.parseInt(maxTokens),
-            temperature: 0,
-          })
-    
-          if (!response) {
-            console.log("response is invalid")
-          } else {
-            if (response.data.choices.length > 0) {
-      
-              response.data.choices.forEach(message => {
-                addMessage(message.text)
-              })
-      
-            } else {
-              console.log("response has no choices")
-            }
-          }
-    
-        } catch (e) {
-          console.log(e.message)
-        }
-      }
-    
-      async function ChatCompletion(openai, model, text, maxTokens, addMessage) {
-        
-        try {
-          const response = await openai.createChatCompletion({
-            model: model,
-            messages: [{role: "user", content: text}],
-          });
-          console.log(response.data.choices[0].message);
-      
-          if (!response) {
-            console.log("response is invalid")
-          } else {
-            if (response.data.choices.length > 0) {
-      
-              response.data.choices.forEach(message => {
-                console.log(message)
-                addMessage(message.message['content'])
-              })
-      
-            } else {
-              console.log("response has no choices")
-            }
-          }
-      
-        } catch (e) {
-          console.log(e.message)
-        }
-    }
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        setIsLoading(true)
-
-        const input = event.target.elements.message;
-        const text = input.value;
-
-        const options = event.target.elements.models;
-        const model = options.value
-
-        const tokens = event.target.elements.maxTokens;
-
-        if (!tokens.value) {
-            tokens.value = 500;
-        }
-
-        const maxTokens = tokens.value;
-
-        if (model === 'gpt-3.5-turbo') {
-          await ChatCompletion(openai, model, text, maxTokens, addMessage)
-        } else {
-          await TextCompletion(openai, model, text, maxTokens, addMessage) 
-        }
-
-        setIsLoading(false)
-      }
+    setIsLoading(false);
+  }
 
   return (
     <div className='chat-container'>
-        {/* <h1 className=''>Chat App</h1> */}
-        <div className='chat-messages'>
-        {messages.map((message, index) => (
-            <div key={index}> <p>{message}</p></div>
-        ))}
-        </div>
-        <form className='chat-form' 
-            onSubmit={handleSubmit}>
-            <textarea className="chat-area" rows={10} placeholder='enter text here' name='message'/>
-            <button type="submit" disabled={isLoading}>Send</button>
-            <button onClick={handleClearMessages} disabled={isLoading}>Clear Messages</button>
-            <select name="models">
-                <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
-                <option value="text-davinci-003">text-davinci-003</option>
-                <option value="text-curie-001">text-curie-001 faster and lower cost</option>
-                <option value="text-babbage-001">text-babbage-001 very fast, and lower cost</option>
-                <option value="text-ada-001">text-ada-001 fastest and lowest cost</option>
-                <option value="code-davinci-002">code-davinci-002 code</option>
-            </select>
-            <input name='maxTokens' placeholder='max tokens'></input>
-        </form>
+      <div className='chat-messages'>
+      {messages.map((message, index) => {
+          if (message.startsWith("```") && message.endsWith("```")) {
+            const code = message.slice(3, -3);
+            return (
+              <pre key={index} className="language-javascript">
+                <code dangerouslySetInnerHTML={{ __html: Prism.highlight(code, Prism.languages.javascript, 'javascript') }} />
+              </pre>
+            );
+          } else {
+            return (
+              <div key={index}>
+                {message.split("\n").map((paragraph, index) => (
+                  <p key={index}>{paragraph}</p>
+                ))}
+              </div>
+            );
+          }
+        })}
+      
+      </div>
+      <form className='chat-form' onSubmit={handleSubmit}>
+        <textarea className='chat-area' rows={6} placeholder='enter text here' name='message'
+          onKeyPress={(e) => {
+          if (e.key === 'Enter') {
+            e.preventDefault();
+            // e.target.form.dispatchEvent(new Event('submit', { cancelable: true }));
+            document.getElementById('send-button').click();
+          }
+        }} />
+        <button type='submit' id='send-button' disabled={isLoading}>
+          Send
+        </button>
+        <button onClick={clearMessages} disabled={isLoading}>
+          Clear Messages
+        </button>
+        <select name='models'>
+          <option value='gpt-3.5-turbo'>gpt-3.5-turbo</option>
+          <option value='text-davinci-003'>text-davinci-003</option>
+          <option value='text-curie-001'>text-curie-001 faster and lower cost</option>
+          <option value='text-babbage-001'>text-babbage-001 very fast, and lower cost</option>
+          <option value='text-ada-001'>text-ada-001 fastest and lowest cost</option>
+          <option value='code-davinci-002'>code-davinci-002 code</option>
+        </select>
+        <input name='maxTokens' placeholder='max tokens' />
+      </form>
     </div>
-  )
+  );
 }
 
 export default Chat;
